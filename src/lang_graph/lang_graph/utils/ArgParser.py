42ad1ade596d7124ae6e231aa42de0f8228da8d9
assume <assume0701@gmail.com>
2024-06-01 13:44:14 +0800

    Update argument parser
    - Preventing parameter parser with ROS
    - Remove unused argument in parser

--- src/lang_graph/lang_graph/utils/ArgParser.py
+++ src/lang_graph/lang_graph/utils/ArgParser.py
@@ -14,29 +14,21 @@ def _get_parser():
     # Image directory
     parser.add_argument("--image_dir", type=str, default="./data/im", help="Directory containing images")
 
-    # Output directory and folder name
-    parser.add_argument("--output_dir", type=str, default="./output", help="Output directory")
-    parser.add_argument("--output_file", type=str, default="output", help="Output file name")
-    
-    # Add arguments for utility functions
-    parser.add_argument("--config_folder", type=str, default="config", help="Folder containing configuration files")
-    parser.add_argument("--config_file", type=str, default="config", help="Configuration file to be used")
+    # args = parser.parse_args()
+    return parser
 
-    args = parser.parse_args()
-    return parser, args.config_file, args.config_folder
-
-def _load_yaml(filename, folder="config"):
-    with open(f"{folder}/{filename}.yaml", "r") as file:
+def _load_yaml(filename):
+    with open(f"{filename}", "r") as file:
         config = yaml.safe_load(file)
     return config
 
-def get_config():
+def get_config(file="config/config.yaml"):
 
     # Get the parser and configuration file
-    parser, config_file, config_folder = _get_parser()
+    parser = _get_parser()
 
     # Load the configuration file from folder
-    config = _load_yaml(config_file, config_folder)
+    config = _load_yaml(file)
 
     # Set the default values to yaml configuration
     parser.set_defaults(**config)
